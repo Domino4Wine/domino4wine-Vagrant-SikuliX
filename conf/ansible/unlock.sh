7c6610c43eb1d1da76390eada813342db271140b
Mark <mark.gilbert@m4kr.net>
2019-05-29 03:49:30 -0500

    Delete unlock.sh

--- conf/ansible/unlock.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-PID=$(pgrep gnome-session)
-export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
-dconf write /org/gnome/desktop/session/idle-delay "0"
