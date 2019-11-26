cce52a51d13da1bea5916f556b7d63be708ab1a9
Mark <mark.gilbert@m4kr.net>
2019-11-25 22:29:50 -0600

    Update logout.sh

--- conf/Desktop/logout.sh
+++ conf/Desktop/logout.sh
@@ -1,2 +1,5 @@
 #/bin/bash
 gnome-session-quit --logout --no-prompt
+#curl domino-server.prominic.net/someapi?Somedata I am done Remove me from the Pool
+#ansible -k playbookthat-cleans-up-user
+#curl domino-server.prominic.net/someapi?Somedata  User has been deleted
