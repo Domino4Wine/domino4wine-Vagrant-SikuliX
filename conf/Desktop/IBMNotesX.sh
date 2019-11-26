7219fca881368015c9ca67b279a0631e66fcaf3f
Mark <mark.gilbert@m4kr.net>
2019-11-25 22:49:21 -0600

    Update IBMNotesX.sh

--- conf/Desktop/IBMNotesX.sh
+++ conf/Desktop/IBMNotesX.sh
@@ -1,2 +1,7 @@
 #/bin/bash
 /opt/cxoffice/bin/wine --bottle Notes10Demo --check --wait-children --start "C:/users/crossover/Start Menu/Programs/IBM Applications/IBM Notes.lnk" 
+#Now we wait for an exit code
+#User has exited, run logout.sh
+#Check if Administrator is open
+/usr/share/applications/logout.sh
+# whoami run asnible script
